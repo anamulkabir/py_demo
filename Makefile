087d329acc8bbc8cb2d8135c6a9d19a002d45b6d
Anamul Kabir <Anamulk@irdinc.com>
2019-12-23 15:11:14 -0600

    first py commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+# init:
+#     pip install -r reauirement.txt
+# test:
+#     py.test tests
+# .PHONY: init test
---
+++ test_unit.py
@@ -0,0 +1,12 @@
+import unittest 
+from  tddmain import *
+
+class FirstTests(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('code'.upper(),'CODE')
+    def test_hello(self):
+        self.assertEqual(hello_world(),'hello world')
+    def test_create_num_list(self):
+        self.assertEqual(create_num_list(10),list(range(0,10)))
+if(__name__ == '__main__'):
+    unittest.main()

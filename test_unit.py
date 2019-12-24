56320b6189b6299663a258f2f4943df674f682a6
Anamul Kabir <Anamulk@irdinc.com>
2019-12-24 13:03:49 -0600

    update test case

--- test_unit.py
+++ test_unit.py
@@ -5,7 +5,7 @@ class FirstTests(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('code'.upper(),'CODE')
     def test_hello(self):
-        self.assertEqual(hello_world(),'hello world jenkins')
+        self.assertEqual(hello_world(),'hello')
     def test_create_num_list(self):
         self.assertEqual(create_num_list(10),list(range(0,10)))
 if(__name__ == '__main__'):

6ec69a6b79b6383d5b781158d233e3258d7fe053
Anamul Kabir <Anamulk@irdinc.com>
2019-12-30 14:53:48 -0600

    test case update

--- test_unit.py
+++ test_unit.py
@@ -7,6 +7,6 @@ class FirstTests(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world(),'Hello World')
     def test_create_num_list(self):
-        self.assertEqual(create_num_list(10),list(range(0,10)))
+        self.assertEqual(create_num_list(9),list(range(0,9)))
 if(__name__ == '__main__'):
     unittest.main()
